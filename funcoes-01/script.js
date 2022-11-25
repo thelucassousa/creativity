b4031bb0fed86d397a1004de52b54f3c2ed91142
Lucas Sousa <luccassous4@outlook.com>
2022-11-24 23:15:27 -0300

    finalizing funcoes-01

---
+++ funcoes-01/script.js
@@ -0,0 +1,17 @@
+const send = document.querySelector('#send');
+
+send.addEventListener('click', function(e){
+    e.preventDefault();
+
+    const ageQuestion = document.querySelector('#ageQuestion');
+    const value = ageQuestion.value;
+
+
+     if(value >= 18){
+        return alert('Welcome!');
+    } else{
+        return alert('Below the legal age!');
+            }
+    
+
+})
